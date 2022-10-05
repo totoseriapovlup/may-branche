8851b2e26866bb0068e649fc278418d0a0aea474
toto <test@test.com>
2022-10-05 16:41:32 +0300

    test

--- index.php
+++ index.php
@@ -9,14 +9,11 @@ spl_autoload_register(function ($className) {
     return false;
 });
 
-
-
-
-
-
-
-//some object for testing
-//k;jkljl
-$test = new Test();
-echo $test;
-unset($test);
+spl_autoload_register(function ($className) {
+    $classFile = 'vendor/' . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+    if(file_exists($classFile)){
+        include_once $classFile;
+        return true;
+    }
+    return false;
+});
